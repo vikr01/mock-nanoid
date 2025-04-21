f1b6f61358463a68b8249c2c5eabc5f40cf64ba6
vikr01 <vik120@icloud.com>
2025-04-21 02:01:05 -0700

    move back to tsc/ts-jest since babel can't generate .d.ts files

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,7 @@ export default {
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     transform: {
-        '^.+\\.(ts|js)$': 'babel-jest',
+        '^.+\\.(ts|js)$': 'ts-jest',
     },
     testPathIgnorePatterns: [
         'node_modules/(?!(nanoid)/)',
